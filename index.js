d4054d64d6ece03ad04a8090bf4484394d0a9bb1
Paweł <qnick.mail@gmail.com>
2016-11-20 18:09:06 +0100

    bower is required - we need more bugfriendly

---
+++ index.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const opn = require('opn');
+const NpmGuiCore = require('npm-gui-base-core');
+const NpmGuiCoreControllers = require('npm-gui-base-core-controllers');
+
+global.appRoot = path.resolve(__dirname);
+const app = express();
+
+
+// Define a port/host we want to listen to
+const PORT = 1337;
+const HOST = '0.0.0.0';
+
+
+// middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+
+// use routes
+app.use('/', NpmGuiCoreControllers.Routes.Static);
+app.use('/api/dependencies', NpmGuiCoreControllers.Routes.Dependencies);
+app.use('/api/dependenciesDev', NpmGuiCoreControllers.Routes.Dependencies);
+app.use('/api/dependenciesBin', NpmGuiCoreControllers.Routes.DependenciesBin);
+app.use('/api/globalPackages', NpmGuiCoreControllers.Routes.GlobalPackages);
+app.use('/api/tasks', NpmGuiCoreControllers.Routes.Tasks);
+app.use('/api/crawler', NpmGuiCoreControllers.Routes.Crawler);
+app.use('/api/project', NpmGuiCoreControllers.Routes.Project);
+
+
+function start(host, port) {
+  // start server
+  const server = app.listen(port || PORT, host || HOST, () => {
+    console.log(`npm-gui web-server running at http://${(host || HOST)}:${(port || PORT)}/`);
+    opn(`http://${(host || HOST)}:${(port || PORT)}`);
+  });
+
+  NpmGuiCore.Service.Console.bind(server);
+
+  return server;
+}
+
+module.exports = start;
